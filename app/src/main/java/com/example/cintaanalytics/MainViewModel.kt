f5f3200a993ec38dae068ca4aa537d91a56a2f7c
Tanuj M <thetanuj@hotmail.com>
2020-08-26 18:31:44 +0530

    Added uid

--- app/src/main/java/com/example/cintaanalytics/MainViewModel.kt
+++ app/src/main/java/com/example/cintaanalytics/MainViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.cintaanalytics
 
 import android.app.Application
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
 import com.example.cintaanalytics.db.Event
+import java.util.*
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    var uid: String
+
+    init {
+        val preferences: SharedPreferences = application.getSharedPreferences("CINTA_PREFS", 0);
+        val userId = preferences.getString("UID", "")
+        if (userId.isNullOrEmpty()) {
+            uid = UUID.randomUUID().toString()
+            preferences.edit().putString("UID", uid).apply()
+        } else {
+            uid = userId
+        }
+    }
 
     private var repository: MainRepository = MainRepository(application)
 
@@ -15,7 +30,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun addEvent(eventName: String) {
-        val event = Event("sdf", eventName, System.currentTimeMillis())
+        val event = Event(uid, eventName, System.currentTimeMillis())
         setEvent(event)
     }
 
